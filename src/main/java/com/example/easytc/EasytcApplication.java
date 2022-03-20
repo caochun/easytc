af66652896d9adedfc70ed38dd9343786d87414b
Chun <caochun@gmail.com>
2022-03-20 11:12:59 +0800

    init

---
+++ src/main/java/com/example/easytc/EasytcApplication.java
@@ -0,0 +1,13 @@
+package com.example.easytc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EasytcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EasytcApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/easytc/EasytcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.easytc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EasytcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
